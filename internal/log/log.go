1b7213ef9cb7b06170508572a28f25779283d278
danblok <danytokmakov@gmail.com>
2024-03-16 17:09:17 +0500

    feat: add basic logger

---
+++ internal/log/log.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	// LOCAL defines the local environment
+	LOCAL = "local"
+
+	// DEV defines the dev environment
+	DEV = "dev"
+
+	// PROD defines the prod environment
+	PROD = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case LOCAL:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case DEV:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case PROD:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
