096b68a78baa709957ff659edea75ee07d640187
danblok <danytokmakov@gmail.com>
2024-03-19 23:54:47 +0500

    feat: add grpc client api for CleanerService

---
+++ internal/grpcapi/grpcapi.go
@@ -0,0 +1,40 @@
+package grpcapi
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/danblok/cleanbg/internal/pb"
+	"github.com/danblok/cleanbg/internal/types"
+)
+
+type cleanerClient struct {
+	client pb.CleanerServiceClient
+}
+
+func Connect(target string) (types.Cleaner, error) {
+	conn, err := grpc.Dial(
+		target,
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(10<<20)),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, err
+	}
+	client := pb.NewCleanerServiceClient(conn)
+	return &cleanerClient{
+		client: client,
+	}, nil
+}
+
+func (c *cleanerClient) Clean(ctx context.Context, image []byte) ([]byte, error) {
+	res, err := c.client.Clean(ctx,
+		&pb.CleanRequest{
+			Data: image,
+		},
+	)
+
+	return res.Data, err
+}
